afbb308b2318b97af8ef8c03bccdfa49b34515c1
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-03-09 16:41:40 -0300

    feat: cadastro

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { signinSchema, signupSchema } from "../schemas/auth.schema.js";
 import { validateSchema } from "../middlewares/validate.schema.js";
+import { createUser } from "../controllers/auth.controllers.js";
 
 const AuthRoutes = Router();
+
+AuthRoutes.post ("signin", validateSchema(signinSchema), () => {})
+AuthRoutes.post ("signup", validateSchema(signupSchema), createUser)
 
 export default AuthRoutes;
