122317aa60ebb9b5302789ffe9a409bd5fecb1e8
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-03-09 10:56:37 -0300

    fix: bugs nas rotas

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { deletePost, updatePost } from "../controllers/deleteupdate.js";
 import { validateToken } from "../middlewares/validate.token.js";
-const router= Router();
 
-router.delete("/timeline", validateToken, deletePost)
-router.update("/timeline", validateToken, updatePost)
-export default router;
+const PostRoutes= Router();
+
+PostRoutes.delete("/timeline", validateToken, deletePost)
+//PostRoutes.update("/timeline", validateToken, updatePost)
+export default PostRoutes;
 
